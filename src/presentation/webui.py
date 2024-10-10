d6c89451679e4951f012249b3cdf164290a0fbad
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2024-10-10 09:46:34 +0200

    fix(webui): use api

--- src/presentation/webui.py
+++ src/presentation/webui.py
@@ -1,9 +1,7 @@
 from flask import Flask, redirect, render_template, request, url_for
-from requests import get
+from requests import get, put
 
-from ..domain.person import Person, update_db_person
-from ..domain.scrape.list_scraper import get_top
-from ..domain.scrape.people_scraper import get_born
+from ..domain.person import Person
 from . import API_URL
 
 app = Flask(__name__)
@@ -11,14 +9,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def list_all():
-    # FIXME: raw_people = get(API_URL).json()["people"]
-    raw_people = get_top("People")
+    raw_people = get(API_URL).json()["people"]
 
     # Validate the data
     people = list(
         map(
-            # FIXME: lambda x: Person(id=x["id"], name=x["name"], born=x["born"]),
-            lambda x: Person(id=x[1], name=x[0], born=get_born(x[1])),
+            lambda x: Person(id=x["id"], name=x["name"], born=x["born"]),
             raw_people,
         )
     )
@@ -27,8 +23,7 @@ def list_all():
 
 @app.get("/<person_id>")
 def get_person(person_id: str):
-    # FIXME: person = get(f"{API_URL}/{person_id}").json()["person"]
-    person = Person(id=person_id, name="John Doe", born="01-01-1970")
+    person = get(f"{API_URL}/{person_id}").json()["person"]
     return render_template("person.html", person=person)
 
 
@@ -39,7 +34,7 @@ def update_person(person_id: str):
         name=request.form["name"],
         born=request.form["born"],
     )
-    update_db_person(person)
+    put(f"{API_URL}/{person_id}", data=person.model_dump_json())
     # Post/Redirect/Get pattern
     # to avoid resubmission of the form
     return redirect(url_for("get_person", person_id=person_id))
